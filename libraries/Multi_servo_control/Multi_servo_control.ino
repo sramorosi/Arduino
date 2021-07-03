6fcec605bb1485e439ce335a64b2eb79a29e8826
sramorosi <sramorosi@users.noreply.github.com>
2021-07-03 12:20:14 -0700

    match new input arm

--- libraries/Multi_servo_control/Multi_servo_control.ino
+++ libraries/Multi_servo_control/Multi_servo_control.ino
@@ -1,9 +1,7 @@
 /*
 Control a robot arm (using Servos) from an Input arm (using Potentiometers)
 
-by SrAmo, May 15, 2021
-
-JOINT C SERVO NOT INSTALLED--NEED ARM REDESIGN
+by SrAmo, July 3, 2021
 
 USE SERIAL PLOTTER TO REVIEW ONE VARIABLE AT A TIME
 
@@ -13,49 +11,57 @@ USE SERIAL PLOTTER TO REVIEW ONE VARIABLE AT A TIME
 // Servo Function library at: http://arduino.cc/en/Reference/Servo
 
 #include "RunningAverage.h"
+// Running Average library at: https://www.arduino.cc/reference/en/libraries/runningaverage/
 
 // Define a running average object for each potentiometer
+// This is used to smooth the readings and to slow down
+//   the arm accelerations, for safety.
 // The integer is the size of the running average list
-#define RA_SIZE 25 // size of the running average list
+//  25 is too slow.  15 is still a bit slow
+#define RA_SIZE 10 // size of the running average list
 RunningAverage potA_RA(RA_SIZE);
 RunningAverage potB_RA(RA_SIZE);
 RunningAverage potC_RA(RA_SIZE);
 RunningAverage potD_RA(RA_SIZE);
 
 // Symbolic Constants:
-#define SERVOS FALSE
+#define JNTA_POT_PIN 5  // Joint A Potentiometer on analog pin A5
+#define JNTB_POT_PIN 4  // Joint B Potentiometer on analog pin A4
+#define JNTC_POT_PIN 3  // Joint C Potentiometer on analog pin A3
+#define JNTD_POT_PIN 2  // Joint D (claw) Potentiometer on analog pin A2
+#define JNTT_POT_PIN 1  // Joint T (table) Potentiometer on analog pin A1
+// A0 not used at this  time
 
-#define JNTA_POT_PIN 0  // Potentiometer on analog pin 0
-#define JNTB_POT_PIN 1  // Potentiometer on analog pin 1
-#define JNTC_POT_PIN 2  // Potentiometer on analog pin 2
-#define JNTD_POT_PIN 3  // Potentiometer on analog pin 3
-
-// Use these constants to tune the potetiometer sensitivity
+// Use these constants to tune the potetiometer RANGE
 // number that ranges from 0 (0 Volts) to 1023 (5 Volts)
-#define JNTA_POT_MIN 0 
-#define JNTB_POT_MIN 0 
-#define JNTC_POT_MIN 60 
-#define JNTD_POT_MIN 630  //   BACKWARD
+#define JNTA_POT_MIN 60 
+#define JNTB_POT_MIN 160 
+#define JNTC_POT_MIN 740 // reverse
+#define JNTD_POT_MIN 720 // reverse
 
-#define JNTA_POT_MAX 680 
-#define JNTB_POT_MAX 690 
-#define JNTC_POT_MAX 900 
-#define JNTD_POT_MAX 300  // BACKWARD
+#define JNTA_POT_MAX 650 
+#define JNTB_POT_MAX 950 
+#define JNTC_POT_MAX 280  // reverse
+#define JNTD_POT_MAX 430  // reverse
 
 // Use these constants to control the range of the servo outputs
 // Values are DEGREES (typically 0 to 180)  
-//    Start small and increase with tuning
-#define JNTA_SVO_MIN 30   // up/back
-#define JNTB_SVO_MIN 30
-#define B_SVO_MIN 160  // BACKWARD
-#define JNTC_SVO_MIN 40
-#define JNTD_SVO_MIN 20
+//    ** Start small and increase with tuning **
+//    ** to avoid current overloads           **
+#define JNTA_SVO_MIN 130   // up/back
+#define JNTB_SVO_MIN 0
+#define B_SVO_MIN 180  //
+#define JNTC_SVO_MIN 20  
+#define JNTD_SVO_MIN 160  
 
-#define JNTA_SVO_MAX 130  // down/out
+#define JNTA_SVO_MAX 30  // down/out
 #define JNTB_SVO_MAX 160
-#define B_SVO_MAX 0   // BACKWARD
-#define JNTC_SVO_MAX 120
-#define JNTD_SVO_MAX 160
+#define B_SVO_MAX 00   //
+#define JNTC_SVO_MAX 140  
+#define JNTD_SVO_MAX 20 
+
+#define SVO_MIN 0 // used with constrain to keep servo value acceptable
+#define SVO_MAX 180 // used with constrain to keep servo value acceptable
 
 // Declare global variables
 
@@ -71,15 +77,16 @@ void setup() // this function runs once when the sketch starts up
 {
   // No need to setup "analog in" pin. 
 
-  // Initialize running average
+  /* Initialize running average: Doesn't seem to work very well.
   potA_RA.clear();
-  potA_RA.fillValue(90,RA_SIZE);  // fillValue(value, nr) adds nr elements of value.
+  potA_RA.fillValue(20,RA_SIZE);  // fillValue(value, nr) adds nr elements of value.
   potB_RA.clear();
-  potB_RA.fillValue(90,RA_SIZE);
+  potB_RA.fillValue(20,RA_SIZE);
   potC_RA.clear();
-  potC_RA.fillValue(90,RA_SIZE);
+  potC_RA.fillValue(20,RA_SIZE);
   potD_RA.clear();
-  potD_RA.fillValue(90,RA_SIZE);
+  potD_RA.fillValue(20,RA_SIZE);
+  */
 
   // Attach tells the Arduino to begin sending control signals
   // to the servo. Servos require a continuous stream of control
@@ -106,7 +113,8 @@ void loop() // this function runs repeatedly after setup() finishes
   int potA,potB,potC,potD;
   int smooth_potA,smooth_potB,smooth_potC,smooth_potD;
   int jntA, jntB, jntC, jntD;
-  int jntB_Mod;
+  int jntB_Mod, jntB_Mod_temp;
+  int B_map_min,B_map_max;
   
   // The potentiometer is set up as a voltage divider, so that
   // when you turn it, the voltage on the center pin will vary
@@ -134,42 +142,55 @@ void loop() // this function runs repeatedly after setup() finishes
   smooth_potC = potC_RA.getAverage();
   smooth_potD = potD_RA.getAverage();
 
-  // Both map the pot value to degrees and constrain the angle, if something maps wrong
-  jntA = constrain(map(smooth_potA,JNTA_POT_MIN,JNTA_POT_MAX,JNTA_SVO_MIN,JNTA_SVO_MAX),JNTA_SVO_MIN,JNTA_SVO_MAX);    
-  jntB = constrain(map(smooth_potB,JNTB_POT_MIN,JNTB_POT_MAX,JNTB_SVO_MIN,JNTB_SVO_MAX),JNTB_SVO_MIN,JNTB_SVO_MAX);    
-  jntC = constrain(map(smooth_potC,JNTC_POT_MIN,JNTC_POT_MAX,JNTC_SVO_MIN,JNTC_SVO_MAX),JNTC_SVO_MIN,JNTC_SVO_MAX);    
-  jntD = constrain(map(smooth_potD,JNTD_POT_MIN,JNTD_POT_MAX,JNTD_SVO_MIN,JNTD_SVO_MAX),JNTD_SVO_MIN,JNTD_SVO_MAX);    
+  // Both map the pot value to degrees and constrain the angle, to prevent current overloads.
+  jntA = constrain(map(smooth_potA,JNTA_POT_MIN,JNTA_POT_MAX,JNTA_SVO_MIN,JNTA_SVO_MAX),SVO_MIN,SVO_MAX);    
+  jntB = constrain(map(smooth_potB,JNTB_POT_MIN,JNTB_POT_MAX,JNTB_SVO_MIN,JNTB_SVO_MAX),SVO_MIN,SVO_MAX);
+  jntC = constrain(map(smooth_potC,JNTC_POT_MIN,JNTC_POT_MAX,JNTC_SVO_MIN,JNTC_SVO_MAX),SVO_MIN,SVO_MAX);    
+  jntD = constrain(map(smooth_potD,JNTD_POT_MIN,JNTD_POT_MAX,JNTD_SVO_MIN,JNTD_SVO_MAX),SVO_MIN,SVO_MAX);    
 
   // Servo B is on joint A, so need to combine A and B
-  jntB_Mod = jntA+jntB;
-  jntB_Mod = map(jntB_Mod,30,300,B_SVO_MIN,B_SVO_MAX);  
+  jntB_Mod_temp = jntA+(160-jntB);
+  //B_map_min = JNTA_POT_MIN + JNTB_POT_MIN+40; // pysically can't get to the min, add 40
+  //B_map_max = JNTA_POT_MAX + JNTB_POT_MAX;
+  //  JOINT B DOES NOT MOVE LIKE THE INPUT ARM.  ALSO HAVE CONSTANTS HERE.
+  jntB_Mod = constrain(map(jntB_Mod_temp,70,220,B_SVO_MIN,B_SVO_MAX),SVO_MIN,SVO_MAX);  
+  //jntB_Mod = constrain(map(jntB_Mod_temp,B_map_min,B_map_max,B_SVO_MIN,B_SVO_MAX),B_SVO_MAX,B_SVO_MIN);  
 
 // use one of these lines with serial plotter to tune the constants
-  //Serial.println(jntA);
+  //Serial.println(smooth_potD);  // change to potB, potC, etc. to see others
+  //Serial.println(jntD); // change to jntA, jntB, jntC, etc to see others
   //Serial.println(jntB_Mod);
-  Serial.println(jntC);
-  //Serial.println(jntD);
 
-  // To control a servo, you give it the angle you'd like it
-  // to turn to. 
-  // Change servo positions:
+  // Change servo positions using write command (degrees):
+  // NOTE: DRIVING PAST ENDPOINTS IS A HIGH CURRENT STATE.
+  //
     servoA.write(jntA);
     servoB.write(jntB_Mod);
     servoC.write(jntC);
     servoD.write(jntD);
+    //
 
-/*
+//
+  // output for debugging
   Serial.print("potA , ");
-  Serial.print(potA);
+  Serial.print(potA,1);  // Serial.print(val,digits)
+  Serial.print(":  jntA, ");
+  Serial.print(jntA,1);
   Serial.print(":  potB, ");
-  Serial.print(potB);
+  Serial.print(potB,1);
   Serial.print(":  jntB, ");
-  Serial.print(jntB);
+  Serial.print(jntB,1);
+  Serial.print(":  jntB_Mod_temp , ");
+  Serial.print(jntB_Mod_temp,1);
+  Serial.print(":  jntB_Mod, ");
+  Serial.print(jntB_Mod,1);
   Serial.print(": potC , ");
-  Serial.print(potC);
+  Serial.print(potC,1);
   Serial.print(": jntC , ");
-  Serial.print(jntC);
-  Serial.print(": JOINT D, ");
-  Serial.println(potD);
-*/
+  Serial.print(jntC,1);
+  Serial.print(": potD, ");
+  Serial.print(potD,1);
+  Serial.print(": jntD , ");
+  Serial.println(jntD,1);
+//
 }
